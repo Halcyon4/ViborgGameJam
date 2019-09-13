bb2171c9c6778072b3cc1f71d67f14b5b7b4e9d4
Unknown <Dasnoresnoredatemays@gmail.com>
2019-09-13 21:39:40 +0200

    Mirror rotation
    Mirror prefab added

---
+++ Unity/Viborg Game Jam Unity/Assets/Scripts/MirrorRotation.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirrorRotation : MonoBehaviour
+{
+
+    public float speed = 0.1f;
+
+    public Transform targetTransform;
+
+    bool rotating;
+    float rotationProgress;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E) && !rotating)
+        {
+            targetTransform.Rotate(transform.up, 45f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q) && !rotating)
+        {
+            targetTransform.Rotate(transform.up, -45f);
+        }
+
+        if (transform.rotation != targetTransform.rotation)
+        {
+            rotating = true;
+
+            rotationProgress += speed * Time.deltaTime;
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetTransform.rotation, rotationProgress);
+        }
+        else
+        {
+            rotating = false;
+            rotationProgress = 0;
+        }
+    }
+}
