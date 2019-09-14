c2f933144bfc095a089d45ae7d08a5ca6c465c9a
Luci4 <stormsoendergaard@gmail.com>
2019-09-15 00:29:44 +0200

    stuff

---
+++ Unity/Viborg Game Jam Unity/Assets/RotRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotRandomizer : MonoBehaviour
+{
+    void Start()
+    {
+        transform.eulerAngles += new Vector3(0,Random.Range(0, 16)*22.5f,0);
+
+    }
+}
