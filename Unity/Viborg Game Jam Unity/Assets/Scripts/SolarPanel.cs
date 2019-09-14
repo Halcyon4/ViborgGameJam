61586ed4094092cef2a573247bfde5718e3991a9
Luci4 <stormsoendergaard@gmail.com>
2019-09-14 16:16:24 +0200

    Level stuff

--- Unity/Viborg Game Jam Unity/Assets/Scripts/SolarPanel.cs
+++ Unity/Viborg Game Jam Unity/Assets/Scripts/SolarPanel.cs
@@ -7,7 +7,7 @@ public class SolarPanel : MonoBehaviour
 
     public bool powered;
 
-    public GameObject sphere;
+    //public GameObject sphere;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,6 @@ public class SolarPanel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        sphere.SetActive(powered);
+        //if()sphere.SetActive(powered);
     }
 }
